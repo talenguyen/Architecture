e0e752096ff211b4434d6c91f4621cda5a51ab9f
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-02-21 21:16:22 +0700

    refactor for better code style

--- app/src/test/java/vn/tale/architecture/repos/menu/bottom/BottomMenuPresenterTest.java
+++ app/src/test/java/vn/tale/architecture/repos/menu/bottom/BottomMenuPresenterTest.java
@@ -1,12 +1,10 @@
-package vn.tale.architecture.repos;
+package vn.tale.architecture.repos.menu.bottom;
 
 import io.reactivex.Observable;
 import org.junit.Before;
 import org.junit.Test;
 import vn.tale.architecture.model.User;
 import vn.tale.architecture.model.manager.UserModel;
-import vn.tale.architecture.repos.menu.bottom.BottomMenuPresenter;
-import vn.tale.architecture.repos.menu.bottom.BottomMenuView;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -16,10 +14,10 @@ import static org.mockito.Mockito.when;
  * Created by Giang Nguyen on 2/21/17.
  */
 public class BottomMenuPresenterTest {
-  UserModel mockedUserModel;
   User mockedUser;
+  UserModel mockedUserModel;
+  BottomMenuView mockedBottomMenuView;
   BottomMenuPresenter tested;
-  private BottomMenuView mockedBottomMenuView;
 
   @Before
   public void setUp() throws Exception {
@@ -29,18 +27,19 @@ public class BottomMenuPresenterTest {
     tested = new BottomMenuPresenter(mockedUserModel);
 
     when(mockedUserModel.user()).thenReturn(Observable.just(UserModel.ANNOYMOUS));
-
-    tested.attachView(mockedBottomMenuView);
   }
 
   @Test
   public void should_hide_user_repos_when_not_login() throws Exception {
+    tested.attachView(mockedBottomMenuView);
+
     verify(mockedBottomMenuView).hideUserRepos();
   }
 
   @Test
-  public void should_show_user_repo_when_user_logged_in() throws Exception {
+  public void should_show_user_repo_when_logged_in() throws Exception {
     when(mockedUserModel.user()).thenReturn(Observable.just(mockedUser));
+
     tested.attachView(mockedBottomMenuView);
 
     verify(mockedBottomMenuView).showUserRepos();
