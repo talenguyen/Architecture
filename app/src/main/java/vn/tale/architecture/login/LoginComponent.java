5bb3c9e796514445c5618de19d13c66502076b95
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-02-21 17:35:20 +0700

    add dependencies

---
+++ app/src/main/java/vn/tale/architecture/login/LoginComponent.java
@@ -0,0 +1,16 @@
+package vn.tale.architecture.login;
+
+import vn.tale.architecture.AppComponent;
+
+/**
+ * Created by Giang Nguyen on 2/21/17.
+ */
+
+public class LoginComponent {
+
+  private final AppComponent appComponent;
+
+  public LoginComponent(AppComponent appComponent) {
+    this.appComponent = appComponent;
+  }
+}
---
+++ app/src/main/java/vn/tale/architecture/repos/ReposComponent.java
@@ -0,0 +1,17 @@
+package vn.tale.architecture.repos;
+
+import vn.tale.architecture.AppComponent;
+
+/**
+ * Created by Giang Nguyen on 2/21/17.
+ */
+
+public class ReposComponent {
+
+  private final AppComponent appComponent;
+
+  public ReposComponent(AppComponent appComponent) {
+    this.appComponent = appComponent;
+  }
+
+}
