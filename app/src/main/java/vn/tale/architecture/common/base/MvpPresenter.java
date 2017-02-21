64c0a73537fc39e168bd4f5d523e44e3d387218d
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-02-21 20:57:39 +0700

    implement disposeOnDetach to handle dispose disposable onDetach

---
+++ app/src/main/java/vn/tale/architecture/common/base/MvpPresenter.java
@@ -0,0 +1,53 @@
+package vn.tale.architecture.common.base;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by Giang Nguyen on 2/21/17.
+ */
+
+public abstract class MvpPresenter<MvpView> {
+
+  private WeakReference<MvpView> viewReference;
+  private CompositeDisposable compositeDisposable;
+
+  public final void attachView(@NonNull MvpView view) {
+    viewReference = new WeakReference<>(view);
+    onViewAttached();
+  }
+
+  public final void detachView() {
+    viewReference.clear();
+    if (compositeDisposable != null) {
+      compositeDisposable.clear();
+      compositeDisposable = null;
+    }
+    onViewDetached();
+  }
+
+  @Nullable protected MvpView getView() {
+    if (viewReference == null) {
+      return null;
+    }
+    return viewReference.get();
+  }
+
+  protected void disposeOnDetach(Disposable disposable) {
+    if (compositeDisposable == null) {
+      compositeDisposable = new CompositeDisposable();
+    }
+    compositeDisposable.add(disposable);
+  }
+
+  protected void onViewAttached() {
+    // No-op
+  }
+
+  protected void onViewDetached() {
+    // No-op
+  }
+}
---
+++ app/src/test/java/vn/tale/architecture/common/base/MvpPresenterTest.java
@@ -0,0 +1,47 @@
+package vn.tale.architecture.common.base;
+
+import io.reactivex.disposables.Disposable;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Giang Nguyen on 2/21/17.
+ */
+public class MvpPresenterTest {
+
+  Object mockedView;
+  Disposable mockedDisposable;
+  MvpPresenter<Object> tested;
+
+  @Before
+  public void setUp() throws Exception {
+    mockedView = mock(Object.class);
+    mockedDisposable = mock(Disposable.class);
+    tested = new MvpPresenter<Object>() {};
+
+    tested.attachView(mockedView);
+    tested.disposeOnDetach(mockedDisposable);
+  }
+
+  @Test
+  public void should_return_view_when_attached() throws Exception {
+    assertThat(tested.getView()).isNotNull();
+  }
+
+  @Test
+  public void should_return_null_when_detached() throws Exception {
+    tested.detachView();
+
+    assertThat(tested.getView()).isNull();
+  }
+
+  @Test
+  public void should_dispose_disposable_when_detach() throws Exception {
+    tested.detachView();
+    verify(mockedDisposable).dispose();
+  }
+}
