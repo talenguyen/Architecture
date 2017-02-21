949394e22c104ae79a535fde1cfd9ee0d1f8cadd
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-02-21 16:50:01 +0700

    implement dependencies

--- app/src/main/java/vn/tale/architecture/App.java
+++ app/src/main/java/vn/tale/architecture/App.java
@@ -1,10 +1,26 @@
 package vn.tale.architecture;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by Giang Nguyen on 2/21/17.
  */
 
 public class App extends Application {
+
+  private AppComponent appComponent;
+
+  public static App get(Context context) {
+    return ((App) context.getApplicationContext());
+  }
+
+  @Override public void onCreate() {
+    super.onCreate();
+    appComponent = new AppComponent();
+  }
+
+  public AppComponent getAppComponent() {
+    return appComponent;
+  }
 }
---
+++ app/src/main/java/vn/tale/architecture/AppComponent.java
@@ -0,0 +1,33 @@
+package vn.tale.architecture;
+
+import vn.tale.architecture.model.manager.RepoModel;
+import vn.tale.architecture.model.manager.UserModel;
+import vn.tale.architecture.utils.EmailValidator;
+
+/**
+ * Created by Giang Nguyen on 2/21/17.
+ */
+
+public class AppComponent {
+
+  private UserModel userModel;
+  private RepoModel repoModel;
+
+  public UserModel provideUserModel() {
+    if (userModel == null) {
+      userModel = new UserModel();
+    }
+    return userModel;
+  }
+
+  public RepoModel provideRepoModel() {
+    if (repoModel == null) {
+      repoModel = new RepoModel();
+    }
+    return repoModel;
+  }
+
+  public EmailValidator emailValidator() {
+    return new EmailValidator();
+  }
+}
