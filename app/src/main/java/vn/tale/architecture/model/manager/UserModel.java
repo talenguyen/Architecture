430bcbf782e0ab4b4d02c342b09c267100fd2b96
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-02-21 21:01:07 +0700

    notify user change when login, logout

--- app/src/main/java/vn/tale/architecture/model/manager/UserModel.java
+++ app/src/main/java/vn/tale/architecture/model/manager/UserModel.java
@@ -1,7 +1,12 @@
 package vn.tale.architecture.model.manager;
 
 import android.support.v4.util.Pair;
+import io.reactivex.Completable;
+import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.BiConsumer;
+import io.reactivex.subjects.BehaviorSubject;
 import java.util.concurrent.Callable;
 import vn.tale.architecture.model.User;
 import vn.tale.architecture.model.exeption.UserNotFoundException;
@@ -12,6 +17,14 @@ import vn.tale.architecture.model.exeption.UserNotFoundException;
 
 public class UserModel {
 
+  public static final User ANNOYMOUS = new User("annoymous@tale.vn", "annoymous");
+
+  private BehaviorSubject<User> userSubject = BehaviorSubject.createDefault(ANNOYMOUS);
+
+  public Observable<User> user() {
+    return userSubject;
+  }
+
   public Single<User> login(final String email, final String password) {
     final Pair<String, String> authInfo = new Pair<>(email, password);
     return Single.fromCallable(new Callable<User>() {
@@ -21,6 +34,20 @@ public class UserModel {
           return MockManager.USER_MAP.get(authInfo);
         }
         throw new UserNotFoundException("user name & password is not matched");
+      }
+    }).doOnEvent(new BiConsumer<User, Throwable>() {
+      @Override public void accept(User user, Throwable throwable) throws Exception {
+        if (user != null) {
+          userSubject.onNext(user);
+        }
+      }
+    });
+  }
+
+  public Completable logout() {
+    return Completable.fromAction(new Action() {
+      @Override public void run() throws Exception {
+        userSubject.onNext(ANNOYMOUS);
       }
     });
   }
