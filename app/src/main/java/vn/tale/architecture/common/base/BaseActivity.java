a0c4b1389cd0c5ce26e096b8729312a2fc7cde16
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-02-22 14:53:04 +0700

    add butter knife

---
+++ app/src/main/java/vn/tale/architecture/common/base/BaseActivity.java
@@ -0,0 +1,24 @@
+package vn.tale.architecture.common.base;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Giang Nguyen on 2/22/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+  private Unbinder unbinder;
+
+  protected void bindViews(Activity activity) {
+    unbinder = ButterKnife.bind(activity);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    unbinder.unbind();
+  }
+}
