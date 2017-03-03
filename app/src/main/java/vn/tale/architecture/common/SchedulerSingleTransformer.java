5f3e8b1547ab758bd3ea0e1133ff3af88e1e9509
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-03-03 14:04:28 +0700

    refactor

--- app/src/main/java/vn/tale/architecture/common/SchedulerSingleTransformer.java
+++ app/src/main/java/vn/tale/architecture/common/SchedulerSingleTransformer.java
@@ -1,9 +1,9 @@
 package vn.tale.architecture.common;
 
+import android.support.annotation.VisibleForTesting;
 import io.reactivex.Single;
 import io.reactivex.SingleSource;
 import io.reactivex.SingleTransformer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -12,19 +12,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public interface SchedulerSingleTransformer {
 
-  SchedulerSingleTransformer IO = new SchedulerSingleTransformer() {
-    @SuppressWarnings("unchecked")
-    @Override public <T> SingleTransformer<T, T> transformer() {
-      return (SingleTransformer<T, T>) new SingleTransformer() {
-        @Override public SingleSource apply(Single upstream) {
-          return upstream.subscribeOn(Schedulers.io())
-              .observeOn(AndroidSchedulers.mainThread());
-        }
-      };
-    }
-  };
-
-  SchedulerSingleTransformer TEST = new SchedulerSingleTransformer() {
+  @VisibleForTesting SchedulerSingleTransformer TEST = new SchedulerSingleTransformer() {
     @SuppressWarnings("unchecked")
     @Override public <T> SingleTransformer<T, T> transformer() {
       return (SingleTransformer<T, T>) new SingleTransformer() {
